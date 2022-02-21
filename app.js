cca6b8f5c4fef454401e151f9af0601b38eabed4
David <david.riquelme.sb@gmail.com>
2022-02-20 21:45:52 -0300

    CReando Login, Registtro funcionando correctamente en phpmyadmin, nodemon sin errores, node sin errores, aplicacion funcionando sin errores

---
+++ app.js
@@ -0,0 +1,76 @@
+// 1 Invocamos express
+const express = require ('express');
+const app = express();
+
+// 2 Seteamos motor de plantilla EJS
+app.set('view engine', 'ejs');
+
+// 3 Ivocamos a dotenv
+const dotenv = require('dotenv');
+dotenv.config({path:'./env/.env'});
+
+// 4 Directorio publico
+app.use('/resources',express.static('public'));
+app.use('/resources',express.static(__dirname + '/public'));
+
+// 5 Seteamos urlencoded para capturar datos del formulario
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// 6 Invocanmos bcryptjs
+const bcryptjs = require('bcryptjs');
+
+// Revisar este codigo para credenciales y sesiones     
+// 7 Invocamos express-session para las Variables de Sesion
+const session = require('express-session');
+app.use(session({
+    secret:'secret',
+    resave: true,
+    saveUninitialized:true
+}));
+
+// 8 Ivocando el modulo de conexion DB
+const connection = require('./database/db');
+
+// 9 Estableciendo las Rutas
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+app.get('/register',(req,res)=>{
+    res.render('register');
+})
+
+
+// 10 - registro
+app.post('/register', async(req,res)=>{
+    const user = req.body.user;
+    const name = req.body.name;
+    const pass = req.body.pass;
+    let passwordHaash = await bcryptjs.hash(pass , 8);
+
+    connection.query('INSERT INTO users_apofis SET ?', {user:user,name:name,pass:passwordHaash},async(error,results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Alta exitosa')
+            res.render('register',{
+                alert: true,
+                alertTitle: "Registro",
+                alertMenssage: "Registro exitoso!",
+                alertIcon: 'success',
+                showConfirmButton: false,
+                time: 1500,
+                ruta: ''
+            })
+        }
+    });
+})
+
+
+app.listen(3000, (req , res)=>{
+    console.log('Server UP running in hhtp://localhost:3000');
+});
+
+//Comandos de pruebas
+
+//console.log(__dirname);
---
+++ database/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect((error)=>{
+    if(error){
+        console.log('El error de conexion a MYSQL es : ' + error);
+    }
+    console.log('Conectado a DB');
+});
+
+module.exports = connection;
